a3f95f16914af65bc8b728371a804517a3a513a9
Katie Wu <katiewu95@gmail.com>
2016-01-23 17:54:09 -0500

    katie

--- popup/popup.js
+++ popup/popup.js
@@ -2,7 +2,7 @@ var api_key = 'f417bd1dfda811597b5c71a5b08536943d927648';
 $.getJSON('https://api.everyblock.com/content/philly/locations/bella-vista/timeline/.json?schema=crime&token=' + api_key, function (events) {
     $.each(events.results, function (j, event_val) {
         var $event = $('<li>');
-        $event.text(event_val.title);
+        $event.text(event_val.location_name);
         $('.event_list').append($event);
     });
 });
